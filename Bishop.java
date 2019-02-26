0f02740be9f84b15917fd4024fd9201c1e3b4ae3
butzda <48020368+butzda@users.noreply.github.com>
2019-02-26 14:34:53 -0500

    Update Bishop.java
    Movement limits placed on bishop

--- Bishop.java
+++ Bishop.java
@@ -2,18 +2,21 @@ package Project3;
 
 public class Bishop extends ChessPiece {
 
-    public Bishop(Player player) {
-        super(player);
-    }
+	public Bishop(Player player) {
+		super(player);
+	}
 
-    public String type() {
-        return "Bishop";
-    }
+	public String type() {
+		return "Bishop";
+	}
+	
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
 
-    public boolean isValidMove(Move move, IChessPiece[][] board) {
-
-        return true;
+		if(move.toRow - move.fromRow == move.toColumn-move.fromColumn||
+		 move.toRow-move.fromRow==(move.toColumn-move.fromColumn)*(-1))
+		return true;
         // More code is needed
-
-    }
+		return false;
+		
+	}
 }
