690a439b1a7511de05af8788fdf9d3e5818adda1
butzda <48020368+butzda@users.noreply.github.com>
2019-02-26 14:40:03 -0500

    Update Rook.java
    movement limitations applied. queen now fully limited

--- Rook.java
+++ Rook.java
@@ -2,25 +2,33 @@ package Project3;
 
 public class Rook extends ChessPiece {
 
-    public Rook(Player player) {
+	public Rook(Player player) {
+		
+		super(player);
+		
+	}
 
-        super(player);
+	public String type() {
+		
+		return "Rook";
+		
+	}
+	
+	// determines if the move is valid for a rook piece
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
 
-    }
 
-    public String type() {
+		boolean valid = true;
 
-        return "Rook";
-
-    }
-
-    // determines if the move is valid for a rook piece
-    public boolean isValidMove(Move move, IChessPiece[][] board) {
-
-        boolean valid = true;
+		if(!(move.toRow - move.fromRow == 0 ||
+		move.toColumn - move.fromColumn == 0))
         // More code is needed
+			valid = false;
+
         return valid;
 
-    }
 
+		
+	}
+	
 }
