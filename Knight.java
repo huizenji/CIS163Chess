1806366073165b98ffb4de2b36a49201f87066dc
butzda <48020368+butzda@users.noreply.github.com>
2019-02-26 14:26:10 -0500

    Update Knight.java
    conditions set to limit possible locations of movement

--- Knight.java
+++ Knight.java
@@ -2,20 +2,49 @@ package Project3;
 
 public class Knight extends ChessPiece {
 
-    public Knight(Player player) {
-        super(player);
-    }
+	public Knight(Player player) {
+		super(player);
+	}
 
-    public String type() {
-        return "Knight";
-    }
+	public String type() {
+		return "Knight";
+	}
 
-    public boolean isValidMove(Move move, IChessPiece[][] board){
+	public boolean isValidMove(Move move, IChessPiece[][] board){
+		boolean valid = true;
 
-        boolean valid = true;
+		//valid move condition for knight: 2 one way, 1 perpindicular
+		//Test for valid movement then invert value to set valid = false
+		// Test for movement within row within range
+		if(!((move.toRow  - move.fromRow == 1 ||
+				move.toRow - move.fromRow == -1||
+				move.toRow  - move.fromRow == 2 ||
+				move.toRow  - move.fromRow == -2)&&
+
+				//Test for movement within column range
+				(move.toColumn  - move.fromColumn == 1 ||
+				move.toColumn - move.fromColumn == -1||
+				move.toColumn  - move.fromColumn == 2 ||
+				move.toColumn  - move.fromColumn == -2) &&
+
+				//Test for ratio of row/column movements
+				((move.toColumn - move.fromColumn) -
+						(move.toRow - move.fromRow) == 1 ||
+				(move.toColumn - move.fromColumn) -
+						(move.toRow - move.fromRow) == -1||
+				(move.toColumn - move.fromColumn) -
+						(move.toRow - move.fromRow) == 3 ||
+				(move.toColumn - move.fromColumn) -
+						(move.toRow - move.fromRow) == -3)))
+			valid = false;
+
+//		if((move.toRow - move.fromRow) - (move.toColumn - move.fromColumn) != 1 &&
+//				(move.toRow - move.fromRow) - (move.toColumn - move.fromColumn) != -1)
+//			valid = false;
+
         // More code is needed
-        return valid;
-
-    }
+		return valid;
+		
+	}
 
 }
