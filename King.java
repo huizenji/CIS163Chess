50526f654b2f07a34380f07fa4a51695c4bc47a5
butzda <48020368+butzda@users.noreply.github.com>
2019-02-26 14:52:21 -0500

    Update King.java
    king's movement now limited, castle allowed but no necessary restrictions for castle applied, rook not coordinated for castle

--- King.java
+++ King.java
@@ -2,17 +2,30 @@ package Project3;
 
 public class King extends ChessPiece {
 
-    public King(Player player) {
-        super(player);
-    }
+	public King(Player player) {
+		super(player);
+	}
 
-    public String type() {
-        return "King";
-    }
-
-    public boolean isValidMove(Move move, IChessPiece[][] board) {
-        boolean valid = true;
+	public String type() {
+		return "King";
+	}
+	
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
+		boolean valid = true;
         // More code is needed
-        return valid;
-    }
+
+		if(move.toRow - move.fromRow > 1 ||
+		move.toRow - move.fromRow < -1||
+		move.toColumn - move.fromColumn > 1 ||
+		move.toColumn - move.fromColumn < -1)
+			valid = false;
+
+		//allow castle
+		if(move.fromColumn == 4)
+			if(move.toRow - move.fromRow == 0 &&
+					(move.toColumn == 2 || move.toColumn == 6))
+			valid = true;
+
+		return valid;
+	}
 }
