6a7ddcf0b35d82be6d2f44433510faf8f84c7977
butzda <48020368+butzda@users.noreply.github.com>
2019-03-12 14:13:46 -0400

    Update King.java

--- King.java
+++ King.java
@@ -2,6 +2,7 @@ package Project3;
 
 public class King extends ChessPiece {
 
+
 	public King(Player player) {
 		super(player);
 	}
@@ -11,6 +12,7 @@ public class King extends ChessPiece {
 	}
 	
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
+
 		boolean valid = true;
         // More code is needed
 
@@ -19,16 +21,43 @@ public class King extends ChessPiece {
 		move.toColumn - move.fromColumn > 1 ||
 		move.toColumn - move.fromColumn < -1)
 			valid = false;
-		
+
+		//Test for castle
+		if(move.fromColumn == 4 && !super.isMoved())
+			if(move.toRow - move.fromRow == 0)
+				//Queen's side castle
+					if(move.toColumn == 2 &&
+					!board[move.toRow][0].isMoved()){
+						valid = true;
+
+						//check for blocking element
+						for(int i = move.fromColumn - 1; i >= move.toColumn - 1;
+							i--){
+							if(board[move.toRow][i] != null)
+								valid = false;
+						}
+
+					}
+				//King's side castle
+					else if(move.toColumn == 6 &&
+					!board[move.fromRow][7].isMoved()){
+						valid = true;
+
+						//check for blocking element
+						for(int i = move.fromColumn + 1; i < move.toColumn;
+						i++){
+							if(board[move.toRow][i] != null)
+								valid = false;
+						}
+					}
+
 		//check to see if the other spot has a piece for the same player
 		if (super.sameTeam(move, board))
 			valid = false;
 
-		//allow castle
-		if(move.fromColumn == 4)
-			if(move.toRow - move.fromRow == 0 &&
-					(move.toColumn == 2 || move.toColumn == 6))
-			valid = true;
+		//if it's a valid move, set the piece to moved
+		if(valid)
+			super.setMoved(true);
 
 		return valid;
 	}
