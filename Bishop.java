dec8ffccd919c5a4c087585a23cea108af828990
huizenji <47357244+huizenji@users.noreply.github.com>
2019-03-19 14:10:30 -0400

    Update Bishop.java
    Adjusted for inCheck() method

--- Bishop.java
+++ Bishop.java
@@ -2,59 +2,60 @@ package Project3;
 
 public class Bishop extends ChessPiece {
 
-	public Bishop(Player player) {
-		super(player);
-	}
+    public Bishop(Player player) {
+        super(player);
+    }
 
-	public String type() {
-		return "Bishop";
-	}
-	
-	public boolean isValidMove(Move move, IChessPiece[][] board) {
+    public String type() {
+        return "Bishop";
+    }
 
-		boolean valid = false;
+    public boolean isValidMove(Move move, IChessPiece[][] board) {
 
-		if(Math.abs(move.toRow - move.fromRow) == 
-				Math.abs(move.toColumn-move.fromColumn))
-		valid = true;
+        boolean valid = false;
 
-		//check to see if the other spot has a piece for the same player
-		if (super.sameTeam(move, board))
-			valid = false;
+        if(Math.abs(move.toRow - move.fromRow) ==
+                Math.abs(move.toColumn-move.fromColumn))
+            valid = true;
 
-		//check for blocking elements in path to target location
-		if(move.toRow > move.fromRow){
-			if(move.toColumn > move.fromColumn) {
-				for(int i = 1; i < move.toRow - move.fromRow; i++)
-					if(board[move.fromRow + i][move.fromColumn + i]
-							!= null)
-						valid = false;
-			}
+        //check to see if the other spot has a piece for the same player
+        if (super.sameTeam(move, board))
+            valid = false;
 
-			if(move.toColumn < move.fromColumn){
-				for(int i = 1; i < move.toRow - move.fromRow; i++)
-					if(board[move.fromRow + i][move.fromColumn - i]
-							!= null)
-						valid = false;
-			}
-		}
-		else{
-			if(move.toColumn > move.fromColumn) {
-				for(int i = 1; i < move.toColumn - move.fromColumn; i++)
-					if(board[move.fromRow - i][move.fromColumn + i]
-							!= null)
-						valid = false;
-			}
+        //check for blocking elements in path to target location
+        if (move.toRow > move.fromRow) {
+            if (move.toColumn > move.fromColumn) {
+                for (int i = 1; i < move.toRow - move.fromRow; i++)
+                    if (move.fromRow + i <= 7 && move.fromColumn + i <= 7)
+                        if (board[move.fromRow + i][move.fromColumn + i]
+                                != null)
+                            valid = false;
+            }
 
-			if(move.toColumn < move.fromColumn){
-				for(int i = 1; i < move.fromColumn - move.toColumn; i++)
-					if(board[move.fromRow - i][move.fromColumn - i]
-							!= null)
-						valid = false;
-			}
-		}
+            if (move.toColumn < move.fromColumn) {
+                for (int i = 1; i < move.toRow - move.fromRow; i++)
+                    if (move.fromRow + i <= 7 && move.fromColumn - i >= 0)
+                        if (board[move.fromRow + i][move.fromColumn - i]
+                                != null)
+                            valid = false;
+            }
+        } else {
+            if (move.toColumn > move.fromColumn) {
+                for (int i = 1; i < move.toColumn - move.fromColumn; i++)
+                    if (move.fromRow - i >= 0 && move.fromColumn + i <= 7)
+                        if (board[move.fromRow - i][move.fromColumn + i]
+                                != null)
+                            valid = false;
+            }
 
-        // More code is needed
-		return valid;
-	}
+            if (move.toColumn < move.fromColumn) {
+                for (int i = 1; i < move.fromColumn - move.toColumn; i++)
+                    if (move.fromRow - i >= 0 && move.fromColumn - i >= 0)
+                    if (board[move.fromRow - i][move.fromColumn - i]
+                            != null)
+                        valid = false;
+            }
+        }
+        return valid;
+    }
 }
