9f1066a93300a4925bf3067b52814ae109a3ee70
butzda <48020368+butzda@users.noreply.github.com>
2019-03-19 09:16:26 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -16,26 +16,18 @@ public class Knight extends ChessPiece {
 		//valid move condition for knight: 2 one way, 1 perpindicular
 		//Test for valid movement then invert value to set valid = false
 		// Test for movement within row within range
-		if(!((move.toRow  - move.fromRow == 1 ||
-				move.toRow - move.fromRow == -1||
-				move.toRow  - move.fromRow == 2 ||
-				move.toRow  - move.fromRow == -2)&&
+		if(!((Math.abs(move.toRow  - move.fromRow) == 1 ||
+				Math.abs(move.toRow  - move.fromRow) == 2)&&
 
 				//Test for movement within column range
-				(move.toColumn  - move.fromColumn == 1 ||
-				move.toColumn - move.fromColumn == -1||
-				move.toColumn  - move.fromColumn == 2 ||
-				move.toColumn  - move.fromColumn == -2) &&
+				(Math.abs(move.toColumn  - move.fromColumn) == 1 ||
+				Math.abs(move.toColumn  - move.fromColumn) == 2) &&
 
 				//Test for ratio of row/column movements
-				((move.toColumn - move.fromColumn) -
-						(move.toRow - move.fromRow) == 1 ||
-				(move.toColumn - move.fromColumn) -
-						(move.toRow - move.fromRow) == -1||
-				(move.toColumn - move.fromColumn) -
-						(move.toRow - move.fromRow) == 3 ||
-				(move.toColumn - move.fromColumn) -
-						(move.toRow - move.fromRow) == -3)))
+				(Math.abs((move.toColumn - move.fromColumn) -
+						(move.toRow - move.fromRow)) == 1 ||
+				Math.abs((move.toColumn - move.fromColumn) -
+						(move.toRow - move.fromRow)) == 3)))
 			valid = false;
 
 		//check to see if the other spot has a piece for the same player
