12799cddec6c0853cfa46fad99c872eb3b4f3bb0
huizenji <47357244+huizenji@users.noreply.github.com>
2019-03-25 14:24:46 -0400

    Add files via upload
    Added our JUnit class with scaffolding

---
+++ ChessTest.java
@@ -0,0 +1,49 @@
+package Project3;
+
+import org.junit.Test;
+
+public class ChessTest {
+
+    @Test
+    public void testPawn(){
+
+    }
+
+    @Test
+    public void testRook(){
+
+    }
+
+    @Test
+    public void testKnight(){
+
+    }
+
+    @Test
+    public void testBishop(){
+
+    }
+
+    @Test
+    public void testQueen(){
+
+    }
+
+    @Test
+    public void testKing(){
+
+    }
+
+    @Test
+    public void testCastling(){
+
+    }
+
+    @Test
+    public void testEnPassant(){
+        
+    }
+
+    // others as necessary
+
+}
