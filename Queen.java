02fa536e409be9cd9b047cf5187c04ff772c469f
butzda <48020368+butzda@users.noreply.github.com>
2019-03-25 21:39:42 -0400

    Update Queen.java
    I literally added a comment explaining why this is not commented.

--- Queen.java
+++ Queen.java
@@ -1,5 +1,12 @@
 package Project3;
 
+/**********************************************************************
+ * This code is strictly unchanged from what was provided to us by the
+ * class instructor. It checks to see if the requested move is valid
+ * for the bishop and for the rook. If it is valid for either one, it 
+ * returns a boolean "True" value. Otherwise, it returns "False".
+ */
+
 public class Queen extends ChessPiece {
 
     public Queen(Player player) {
