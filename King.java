1ab3439d4bc8e58e9794ef7379667feed2ace8ce
butzda <48020368+butzda@users.noreply.github.com>
2019-03-25 21:43:57 -0400

    Update King.java

--- King.java
+++ King.java
@@ -1,5 +1,12 @@
 package Project3;
 
+/**********************************************************************
+ * A class that stores the information in, and the movement restriction
+ * of a King.
+ *
+ * @author David Butz, Lauren Freeman, Jillian Huizenga
+ * Date: 3/26/2019
+ *********************************************************************/
 public class King extends ChessPiece {
 
 
