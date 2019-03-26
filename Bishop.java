d7424a42f4304c4eb97e2db72aeaa67e9e2bb48f
butzda <48020368+butzda@users.noreply.github.com>
2019-03-25 21:23:11 -0400

    Update Bishop.java
    Bishop is now fully commented

--- Bishop.java
+++ Bishop.java
@@ -2,60 +2,77 @@ package Project3;
 
 public class Bishop extends ChessPiece {
 
-    public Bishop(Player player) {
-        super(player);
-    }
+	public Bishop(Player player) {
+		super(player);
+	}
 
-    public String type() {
-        return "Bishop";
-    }
+	public String type() {
+		return "Bishop";
+	}
 
-    public boolean isValidMove(Move move, IChessPiece[][] board) {
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
 
-        boolean valid = false;
+		boolean valid = false;
 
-        if(Math.abs(move.toRow - move.fromRow) ==
-                Math.abs(move.toColumn-move.fromColumn))
-            valid = true;
+		//Check to ensure movement is on pure diagonal
+		if(Math.abs(move.toRow - move.fromRow) ==
+				Math.abs(move.toColumn-move.fromColumn))
+			valid = true;
 
-        //check to see if the other spot has a piece for the same player
-        if (super.sameTeam(move, board))
-            valid = false;
+	//check to see if the other spot has a piece for the same player
+		if (super.sameTeam(move, board))
+			valid = false;
 
-        //check for blocking elements in path to target location
-        if (move.toRow > move.fromRow) {
-            if (move.toColumn > move.fromColumn) {
-                for (int i = 1; i < move.toRow - move.fromRow; i++)
-                    if (move.fromRow + i <= 7 && move.fromColumn + i <= 7)
-                        if (board[move.fromRow + i][move.fromColumn + i]
-                                != null)
-                            valid = false;
-            }
+		//check for blocking elements in path to target location
+		//Check for movement from left toward right
+		if (move.toRow > move.fromRow) {
+			//check for movement in the upper diagonal
+			if (move.toColumn > move.fromColumn) {
+				//check every space for a piece between start
+				//and stop locations
+				for (int i = 1; i < move.toRow - move.fromRow; i++)
+					if (move.fromRow + i<=7 && move.fromColumn + i<=7)
+						if (board[move.fromRow + i][move.fromColumn + i]
+								!= null)
+							valid = false;
+			}
 
-            if (move.toColumn < move.fromColumn) {
-                for (int i = 1; i < move.toRow - move.fromRow; i++)
-                    if (move.fromRow + i <= 7 && move.fromColumn - i >= 0)
-                        if (board[move.fromRow + i][move.fromColumn - i]
-                                != null)
-                            valid = false;
-            }
-        } else {
-            if (move.toColumn > move.fromColumn) {
-                for (int i = 1; i < move.toColumn - move.fromColumn; i++)
-                    if (move.fromRow - i >= 0 && move.fromColumn + i <= 7)
-                        if (board[move.fromRow - i][move.fromColumn + i]
-                                != null)
-                            valid = false;
-            }
+			//check for movement in the lower diagonal
+			if (move.toColumn < move.fromColumn) {
+				//check every space for a piece between start
+				//and stop locations
+				for (int i = 1; i < move.toRow - move.fromRow; i++)
+					if (move.fromRow + i <= 7 && move.fromColumn - i >= 0)
+						if (board[move.fromRow + i][move.fromColumn - i]
+								!= null)
+							valid = false;
+			}
+		// Check movement from right to left	
+		} else {
+			//check for blocking elements in path to target location
+			//Check for movement from left toward right
+			if (move.toColumn > move.fromColumn) {
+				//check every space for a piece between start
+				//and stop locations
+				for (int i = 1; i < move.toColumn - move.fromColumn; i++)
+					if (move.fromRow - i >= 0 && move.fromColumn + i <= 7)
+						if (board[move.fromRow - i][move.fromColumn + i]
+								!= null)
+							valid = false;
+			}
 
-            if (move.toColumn < move.fromColumn) {
-                for (int i = 1; i < move.fromColumn - move.toColumn; i++)
-                    if (move.fromRow - i >= 0 && move.fromColumn - i >= 0)
-                    if (board[move.fromRow - i][move.fromColumn - i]
-                            != null)
-                        valid = false;
-            }
-        }
-        return valid;
-    }
+			//check for blocking elements in path to target location
+			//Check for movement from right toward left
+			if (move.toColumn < move.fromColumn) {
+				//check every space for a piece between start
+				//and stop locations
+				for (int i = 1; i < move.fromColumn - move.toColumn; i++)
+					if (move.fromRow - i >= 0 && move.fromColumn - i >= 0)
+						if (board[move.fromRow - i][move.fromColumn - i]
+								!= null)
+							valid = false;
+			}
+		}
+		return valid;
+	}
 }
