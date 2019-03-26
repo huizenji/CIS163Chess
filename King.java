585834a874cc03c4160de7ddf29ab6c7f44d33ac
butzda <48020368+butzda@users.noreply.github.com>
2019-03-25 21:30:17 -0400

    Update King.java
    King is now fully commented

--- King.java
+++ King.java
@@ -14,23 +14,27 @@ public class King extends ChessPiece {
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
 
 		boolean valid = true;
-        // More code is needed
 
+		//Test against movements larger than 1 space
 		if(Math.abs(move.toRow - move.fromRow) > 1 ||
 		Math.abs(move.toColumn - move.fromColumn) > 1)
 			valid = false;
 
 		//Test for castle
-		if(move.fromColumn == 4 && !super.isMoved())
+		//If the king has not yet moved...
+		if(!super.isMoved())
+			//Test to make sure King is not leaving base row
 			if(move.toRow - move.fromRow == 0)
 				//Queen's side castle
 					if(move.toColumn == 2 &&
+					//... and the rook has not moved
 					!board[move.toRow][0].isMoved()){
 						valid = true;
 
 						//check for blocking element
-						for(int i = move.fromColumn - 1; i >= move.toColumn - 1;
-							i--){
+						for(int i = move.fromColumn - 1;
+							i >= move.toColumn - 1;	i--){
+							//if there is a piece in the way, not valid
 							if(board[move.toRow][i] != null)
 								valid = false;
 						}
@@ -38,18 +42,20 @@ public class King extends ChessPiece {
 					}
 				//King's side castle
 					else if(move.toColumn == 6 &&
+					//... and the rook has not moved
 					!board[move.fromRow][7].isMoved()){
 						valid = true;
 
 						//check for blocking element
-						for(int i = move.fromColumn + 1; i < move.toColumn;
-						i++){
+						for(int i = move.fromColumn + 1;
+							i < move.toColumn; i++){
+							//if there is a piece in the way, not valid
 							if(board[move.toRow][i] != null)
 								valid = false;
 						}
 					}
 
-		//check to see if the other spot has a piece for the same player
+	//check to see if the other spot has a piece for the same player
 		if (super.sameTeam(move, board))
 			valid = false;
 
