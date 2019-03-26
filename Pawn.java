7d85eb0794f33833c708a3c5780d01014e4109ee
butzda <48020368+butzda@users.noreply.github.com>
2019-03-25 21:17:31 -0400

    Update Pawn.java
    Pawn is now fully commented

--- Pawn.java
+++ Pawn.java
@@ -2,92 +2,107 @@ package Project3;
 
 public class Pawn extends ChessPiece {
 
-    public Pawn(Player player) {
-        super(player);
-    }
+	public Pawn(Player player) {
+		super(player);
+	}
 
-    public String type() {
-        return "Pawn";
-    }
+	public String type() {
+		return "Pawn";
+	}
 
-    // determines if the move is valid for a pawn piece
-    public boolean isValidMove(Move move, IChessPiece[][] board) {
+	// determines if the move is valid for a pawn piece
+	public boolean isValidMove(Move move, IChessPiece[][] board) {
 
-        boolean valid = true;
-        // More code is needed
+		boolean valid = true;
+		
+	//check to see if the other spot has a piece for the same player
+		if (super.sameTeam(move, board))
+			valid = false;
 
-        //check to see if the other spot has a piece for the same player
-        if (super.sameTeam(move, board))
-            valid = false;
+		//Place limitations on white pawns
+		if (super.player() == Player.WHITE) {
+			/**check for diagonal movement. if it exists and there is no
+			 * enemy player, invalid move this is for attacking other
+			 * pieces **/
+			
+			//is this a valid diagonal
+			if (Math.abs(move.toColumn - move.fromColumn) == 1) {
+				//check for enemy player
+				if (board[move.toRow][move.toColumn] != null) {    
+					if (board[move.toRow][move.toColumn].player()
+							== Player.BLACK) {
+						//if an enemy player exists, it's valid
+						valid = true;
+					}
+				} else
+					//if nobody is on the spot, invalid move
+					valid = false;
+				
+				//no other diagonal allowed	
+			} else if (move.toColumn != move.fromColumn)    
+				valid = false;
 
-        //Place limitations on white pawns
-        if (super.player() == Player.WHITE) {
-            //check for diagonal movement. if it exists and there is no enemy player, invalid move
-            //this is for attacking other pieces
-            if ((move.toColumn - move.fromColumn) == 1 ||
-                    (move.toColumn - move.fromColumn) == -1) {        //is this a valid diagonal
-                if (board[move.toRow][move.toColumn] != null) {    //check for enemy player
-                    if (board[move.toRow][move.toColumn].player() == Player.BLACK) {
-                        valid = true;            //if an enemy player exists, it's valid
-                    }
-                } else
-                    valid = false;        //if nobody is on the spot, invalid move
-            } else if (move.toColumn != move.fromColumn)    //no other diagonal allowed
-                valid = false;
+		//check for a piece of any kind in front and prevent movement
+			if (move.fromRow - 2 >= 0)
+				if ((board[move.fromRow - 1][move.toColumn] != null ||
+						board[move.fromRow - 2][move.toColumn] != null)
+						&& move.toColumn == move.fromColumn)
+					valid = false;
 
-            //check for a piece of any kind in front and prevent movement
-            if (move.fromRow - 2 >= 0)
-                if ((board[move.fromRow - 1][move.toColumn] != null ||
-                        board[move.fromRow - 2][move.toColumn] != null)
-                        && move.toColumn == move.fromColumn)
-                    valid = false;
+			/**normal movements**/
+			//no movement beyond 2 spaces allowed
+			if (move.fromRow - move.toRow > 2)
+				valid = false;
 
-            //normal movements
-            //no movement beyond 2 spaces allowed
-            if (move.fromRow - move.toRow > 2)
-                valid = false;
+			//if the pawn has moved, only allow 1 space
+			if (super.isMoved() && move.fromRow - move.toRow != 1)
+				valid = false;
+		}
 
-            //if the pawn has moved, only allow 1 space
-            if (super.isMoved() && move.fromRow - move.toRow != 1)
-                valid = false;
-        }
+		//Place limitations on black pawns
+		if (super.player() == Player.BLACK) {
+			/**check for diagonal movement. if it exists and there is no
+			 * enemy player, invalid move this is for attacking other
+			 * pieces **/
+			
+			//is this a valid diagonal
+			if ((move.toColumn - move.fromColumn) == 1 ||
+					(move.toColumn - move.fromColumn) == -1) {
+				//check for enemy player
+				if (board[move.toRow][move.toColumn] != null) {    
+					if (board[move.toRow][move.toColumn].player()
+							== Player.WHITE) {
+						//if an enemy player exists, it's valid
+						valid = true;            
+					}
+				} else
+					//if nobody is on the spot, invalid move
+					valid = false;
+				//no other diagonal allowed	
+			} else if (move.toColumn != move.fromColumn)    
+				valid = false;
 
-        //Place limitations on black pawns
-        if (super.player() == Player.BLACK) {
-            //check for diagonal movement. if it exists and there is no enemy player, invalid move
-            //this is for attacking other pieces
-            if ((move.toColumn - move.fromColumn) == 1 ||
-                    (move.toColumn - move.fromColumn) == -1) {        //is this a valid diagonal
-                if (board[move.toRow][move.toColumn] != null) {    //check for enemy player
-                    if (board[move.toRow][move.toColumn].player() == Player.WHITE) {
-                        valid = true;            //if an enemy player exists, it's valid
-                    }
-                } else
-                    valid = false;        //if nobody is on the spot, invalid move
-            } else if (move.toColumn != move.fromColumn)    //no other diagonal allowed
-                valid = false;
+			//check for a piece of any kind in front and prevent movement
+			if (move.fromRow + 2 <= 7)
+				if ((board[move.fromRow + 1][move.toColumn] != null ||
+						board[move.fromRow + 2][move.toColumn] != null)
+						&& move.toColumn == move.fromColumn)
+					valid = false;
 
-            //check for a piece of any kind in front and prevent movement
-            if (move.fromRow + 2 <= 7)
-                if ((board[move.fromRow + 1][move.toColumn] != null ||
-                        board[move.fromRow + 2][move.toColumn] != null)
-                        && move.toColumn == move.fromColumn)
-                    valid = false;
+			//normal movements
+			//no movement beyond 2 spaces allowed
+			if (move.fromRow - move.toRow < -2)
+				valid = false;
 
-            //normal movements
-            //no movement beyond spaces allowed
-            if (move.fromRow - move.toRow < -2)
-                valid = false;
+			//if the pawn has moved, only allow 1 space
+			if (super.isMoved() && move.fromRow - move.toRow != -1)
+				valid = false;
+		}
 
-            //if the pawn has moved, only allow 1 space
-            if (super.isMoved() && move.fromRow - move.toRow != -1)
-                valid = false;
-        }
+		//if it's a valid move, set the piece to moved
+		if (valid)
+			super.setMoved(true);
 
-        //if it's a valid move, set the piece to moved
-        if (valid)
-            super.setMoved(true);
-
-        return valid;
-    }
+		return valid;
+	}
 }
